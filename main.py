b0d9505954d9a11239b2a46a04593c0a88dbebd8
Krake24 <krake24@mail.de>
2023-02-05 18:26:40 +0100

    removed discord bot and converted to different database setup

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import os
 import json
-import disnake
-import threading
-from disnake.ext import commands
-from replit import db
 from functools import wraps
 from flask import request, abort, Flask
 import requests
-
-bot = commands.Bot(command_prefix=commands.when_mentioned)
-
-osAssetUrl = "https://opensea.io/assets/0x753f10598c026e73182ca74ed33de05974b9f083/"
-caArtUrl = "https://www.champions.io/pets/nfts/art/"
-website = "https://prime-utils.web.app/"
+from sqlitedict import SqliteDict
 
 f = open("collection.json", "r")
 all_pets = json.loads(f.read())
-
 
 def convertOfferKeysFromDb(old):
     return {
@@ -55,9 +44,6 @@ def convertNeedKeys(old):
     }
 
 
-app = Flask(__name__)
-
-
 def read_user(view_function):
     @wraps(view_function)
     async def decorated_function(*args, **kwargs):
@@ -65,7 +51,7 @@ def read_user(view_function):
         if not state:
             abort(400, "no state given")
         try:
-          user_info = requests.get("https://discord-auth.krake24.repl.co/me?state=" + state).json()
+          user_info = requests.get("https://ot3d-bot-prod-d3bot-s29oax.mo4.mogenius.io/me?state=" + state).json()
         except:
             abort(401, "Unauthorized")
         if not user_info:
@@ -75,13 +61,9 @@ def read_user(view_function):
 
     return decorated_function
 
+app = Flask(__name__)
 
-@app.route('/')
-async def main():
-    return 'alive'
-
-
-@app.route('/offers')
+@app.route('/petmarket/offers')
 async def get_offers():
     result = []
     for offer in db['offers']:
@@ -89,7 +71,7 @@ async def get_offers():
     return json.dumps(result, ensure_ascii=False).encode('utf8')
 
 
-@app.route('/offers/<id>', methods=['POST', 'DELETE'])
+@app.route('/petmarket/offers/<id>', methods=['POST', 'DELETE'])
 @read_user
 async def post_offer(id, user_id, user):
     print(request.method + " offer " + str(id))
@@ -100,7 +82,7 @@ async def post_offer(id, user_id, user):
         result = await remove_pet_offer(int(user_id), int(id))
     return json.dumps(result, ensure_ascii=False).encode('utf8')
 
-@app.route('/needs', methods=['GET'])
+@app.route('/petmarket/needs', methods=['GET'])
 async def get_needs():
     result = []
     for need in db['needs']:
@@ -108,7 +90,7 @@ async def get_needs():
     return json.dumps(result, ensure_ascii=False).encode('utf8')
 
 
-@app.route('/needs', methods=['POST', 'DELETE'])
+@app.route('/petmarket/needs', methods=['POST', 'DELETE'])
 @read_user
 async def post_needs(user_id, user):
     print(request.method + " need " + str(id))
@@ -137,33 +119,6 @@ if 'offers' not in db:
 if 'needs' not in db:
     db['needs'] = []
 
-
-@bot.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
-
-
-@bot.slash_command()
-async def pet(inter):
-    pass
-
-
-@pet.sub_command(description="Prints Website URL", name="website")
-async def get_website(inter):
-    await inter.response.send_message(website)
-
-
-@pet.sub_command(description="Registered a pet as being offered for trade")
-async def offer(inter, id: commands.Range[1, 22238]):
-
-    user_id = inter.user.id
-    user = str(inter.user)
-
-    try:
-        result = await offer_pet(user_id, user, id)
-        await inter.response.send_message(result['message'])
-    except Exception as e:
-        return await inter.response.send_message(str(e))
 
 async def map_to_offer(id, user, user_id, pet):
     offer = {}
@@ -253,33 +208,6 @@ async def remove_pet_offer(user_id, pet_id):
     return result
 
 
-Family = commands.option_enum(
-    ["Any", "Xiva", "Geckoid", "Cramster", "Sumonot", "Papiro"])
-Favorite_Family = commands.option_enum([
-    "Any", "Aos", "Darulk", "Fenrir", "Gatekeeper", "Grondal", "Il’gra",
-    "Karkadon", "Keymaster", "Saadari", "Seris", "Vitra", "Whisperer"
-])
-House_Banner = commands.option_enum([
-    "Any",
-    "⛰️  Earth",
-    "🔮  Arcane",
-    "💀  Death",
-    "🌪️  Air",
-    "🌊   Water",
-    "🔥  Fire",
-    "🌱   Life",
-])
-
-
-@pet.sub_command(description="Search all offered Pets excluding your own")
-async def search(inter, family: Family, house_banner: House_Banner,
-                 favorite_family: Favorite_Family):
-
-    await inter.response.send_message(await search_pet(inter.user.id, family,
-                                                       house_banner,
-                                                       favorite_family))
-
-
 async def search_pet(user_id, family, house_banner, favorite_family):
 
     search_results = db['offers']
@@ -316,16 +244,6 @@ async def search_pet(user_id, family, house_banner, favorite_family):
     return result
 
 
-@pet.sub_command(description="Register a need for a certain pet Type")
-async def need(inter, family: Family, house_banner: House_Banner,
-               favorite_family: Favorite_Family):
-
-    user_id = inter.user.id 
-    user = str(inter.user)
-                 
-    result = await need_pet(user_id, user, family, house_banner, favorite_family)
-    await inter.response.send_message(result['message'])
-
 async def need_pet(user_id, user, family, house_banner, favorite_family):
     result = {}
     search_results = db['needs']
@@ -361,15 +279,6 @@ async def need_pet(user_id, user, family, house_banner, favorite_family):
     return result
 
 
-@remove.sub_command(description="Remove listed pet", name="need")
-async def remove_need(inter, family: Family, house_banner: House_Banner,
-                      favorite_family: Favorite_Family):
-
-    user_id = inter.user.id                    
-                        
-    result = await remove_pet_need(user_id, family, house_banner, favorite_family)
-    await inter.response.send_message(result)
-
 async def remove_pet_need(user_id, family, house_banner, favorite_family):
     search_results = db['needs']
 
@@ -397,42 +306,4 @@ async def remove_pet_need(user_id, family, house_banner, favorite_family):
                 'user'] + ") removed\n"
     return result
 
-
-@pet.sub_command(description="Show values of pet with given ID")
-async def show(inter, id: commands.Range[1, 22238]):
-    pet = next(filter(lambda p: p['id'] == id, all_pets))
-    embed = disnake.Embed(title="Pet #" + str(id),
-                          url=osAssetUrl + str(id),
-                          colour=0x710193)
-    embed.set_image(url=caArtUrl + str(id) + "/pfp.png")
-    embed.add_field(name="Family", value=pet['Family'], inline=True)
-    embed.add_field(name="House Banner",
-                    value=pet['House Banner'],
-                    inline=True)
-    embed.add_field(name="Favorite Family",
-                    value=pet['Favorite Family'],
-                    inline=True)
-    embed.add_field(name="Personality", value=pet["Personality"], inline=True)
-    embed.add_field(name="Favorite Toy",
-                    value=pet['Favorite Toy'],
-                    inline=True)
-    embed.add_field(name="Favorite Food",
-                    value=pet['Favorite Food'],
-                    inline=True)
-    embed.add_field(name="Wallpaper",
-                    value="[PFP](" + caArtUrl + str(id) + "/pfp.png)",
-                    inline=True)
-    embed.add_field(name="Video",
-                    value="[Video](" + caArtUrl + str(id) + "/nft.mp4)",
-                    inline=True)
-    await inter.response.send_message(embed=embed)
-
-
-if __name__ == "__main__":
-    threading.Thread(target=lambda: app.run(
-        host='0.0.0.0', port=5000, debug=True, use_reloader=False)).start()
-try:
-    bot.run(os.environ['botToken'])
-except disnake.errors.HTTPException as e:
-    print(e.response)
-    print(e)
+app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
